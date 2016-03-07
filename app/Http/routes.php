6b3061a14800cdad6de6928df3be902cf63fdc8d
Dennis <dennisslimmers@live.nl>
2016-03-07 09:45:12 +0100

    Added default auth routes, removed link to register page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,7 +31,15 @@ Route::group(['middleware' => ['web']], function () {
 });
 
 Route::group(['middleware' => 'web'], function () {
-    Route::auth();
+    // Authentication Routes...
+    $this->get('login', 'Auth\AuthController@showLoginForm');
+    $this->post('login', 'Auth\AuthController@login');
+    $this->get('logout', 'Auth\AuthController@logout');
+
+    // Password Reset Routes...
+    $this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+    $this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+    $this->post('password/reset', 'Auth\PasswordController@reset');
 
     Route::get('/home', 'HomeController@index');
 });
