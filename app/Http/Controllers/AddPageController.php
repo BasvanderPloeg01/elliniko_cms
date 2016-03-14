04b329cd8380abb15e74a4f63cf0683269c94b4f
Dennis <dennisslimmers@live.nl>
2016-03-14 13:34:36 +0100

    Added auth constructor to controllers

--- app/Http/Controllers/AddPageController.php
+++ app/Http/Controllers/AddPageController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 class AddPageController extends Controller {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    
     public function index() {
         return view('add_page');
     }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+class HomeController extends Controller {
+    public function __construct() {
         $this->middleware('auth');
     }
 
