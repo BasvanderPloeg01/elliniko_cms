868dfc0f6e94d6dcf5d52b55bc2d47f138a70b8b
Dennis <dennisslimmers@live.nl>
2016-04-14 12:06:22 +0200

    Changed code editing permissions

--- app/Http/Controllers/CodeEditorController.php
+++ app/Http/Controllers/CodeEditorController.php
@@ -12,42 +12,14 @@ class CodeEditorController extends Controller {
 	}
 
 	public function index() {
-		$HTMLdir = '../resources/views/pages';
-		$JSdir = '../public/js';
 		$CSSdir = '../public/css';
 
-		if (!is_dir($HTMLdir) || !is_dir($JSdir) || !is_dir($CSSdir)) {
+		if (!is_dir($CSSdir)) {
 			exit('Invalid diretory path');
 		}
 		
-		$HTMLfiles = array();
-		$JSfiles = array();
 		$CSSfiles = array();
 		
-		foreach (scandir($HTMLdir) AS $file) {
-			if ('.' === $file) {
-				continue;
-			}
-
-			if ('..' === $file) {
-				continue;
-			}
-			
-			$HTMLfiles[] = $file;
-		}
-
-		foreach (scandir($JSdir) AS $file) {
-			if ('.' === $file) {
-				continue;
-			}
-
-			if ('..' === $file) {
-				continue;
-			}
-			
-			$JSfiles[] = $file;
-		}
-
 		foreach (scandir($CSSdir) AS $file) {
 			if ('.' === $file) {
 				continue;
@@ -60,6 +32,6 @@ class CodeEditorController extends Controller {
 			$CSSfiles[] = $file;
 		}
 		
-		return view('code_editor', compact('HTMLfiles', 'JSfiles', 'CSSfiles'));
+		return view('code_editor', compact('CSSfiles'));
 	}
 }
--- resources/views/code_editor.blade.php
+++ resources/views/code_editor.blade.php
@@ -4,20 +4,6 @@
     <div class="container content">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <h1>HTML: </h1>
-                <?php foreach($HTMLfiles as $file) { ?>
-                         <a href="{{ url('edit_code?page='.$file) }}" class="list-group-item">
-                             <?= $file ?>
-                         </a>
-                <?php } ?>
-                
-                <h1>Javascript: </h1>
-                <?php foreach($JSfiles as $file) { ?>
-                         <a href="{{ url('edit_code?page='.$file) }}" class="list-group-item">
-                             <?= $file ?>
-                         </a>
-                <?php } ?>
-                
                 <h1>Css: </h1>
                 <?php foreach($CSSfiles as $file) { ?>
                       <a href="{{ url('edit_code?page='.$file) }}" class="list-group-item">
